791e848bf33eb50f6e4546e79b8b3c1094f0598e
Elizaveta Battalova <elizaveta.battalova@rambler.ru>
2025-06-18 19:03:49 +0300

    add basic to do list

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,73 +4,31 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-const [name, setName] = useState('');
-const [message, setMessage] = useState('')
+const [tasks, setTasks] = useState([]);
+const [newTask, setNewTask] = useState('');
 
-const [age, setAge] = useState('');
-const [messageAge, setMessageAge] = useState('');
+const addTask = () => {
+  if(newTask.trim() !== '') {
+    setTasks([...tasks, newTask.trim()]);
+    setNewTask('');
+  }
+};
 
-const [apples, setApples] = useState('');
-const [friends,setFriends] = useState('');
-const [friendMessage, setFriendMessage] = useState('')
+const deleteTask = (indexToDelete) => {
+  setTasks(tasks.filter((_, index) => (index !== indexToDelete)));
+};
 
-const handleInput = (setter) => (e) => {
-  setter(e.target.value)
-}
-
-const buttonName = () => {
-  if (name === '') {
-setMessage(`what is your name?`);
-} else if (name === 'Лиза') {
-  setMessage(`Привет, ${name}, ты большая молодец !`);
-} else {
-  setMessage(`Здравствуй, ${name}`);
-}
-}
-
-const buttonAge = () => {
-  if (age === '') {
-  setMessageAge(`How old are you?`);
-} else if (age >= 18) {
-  setMessageAge(`Adult`);
-} else {
-  setMessageAge(`Child`);
-}
-}
-
-const calculator = () => {
-if(apples === '' || friends === '') {
- setFriendMessage(`enter apples and friends`)
- return;
-}
-let remaining = Number(apples) - Number(friends);
-
-if(remaining === 0) {
-  setFriendMessage(`no apples left`);
-} else if (remaining < 0) {
-  setFriendMessage(`not enough apples`);
-} else {
-  setFriendMessage(`you have ${remaining} apples`);
-}
-}
-
-return (
+return(
   <div>
-    <input type ='text' placeholder = 'your name' value = {name} onChange = {handleInput(setName)}></input>
-    <button onClick = {buttonName}>send name</button>
-    <p>{message}</p>
-
-    <input type = 'number' placeholder = 'your age' value = {age} onChange = {handleInput(setAge)}></input>
-    <button onClick = {buttonAge}>send age</button>
-    <p>{messageAge}</p>
-
-    <input type = 'number' placeholder = 'apples' value = {apples} onChange = {handleInput(setApples)}></input>
-    <input type = 'number' placeholder = 'friends' value = {friends} onChange = {handleInput(setFriends)}></input>
-    <button onClick = {calculator}>send apples</button>
-    <p>{friendMessage}</p>
-
-    <button onClick = {() => {alert('Hello!')}}>hello button</button>
+    <h1>to do list</h1>
+    <input type = 'text' placeholder = 'add task' value = {newTask} onChange = {(e) => setNewTask(e.target.value)}></input>
+    <button onClick = {addTask}>add</button>
+    <ul>
+          {tasks.map((task, index) => (
+      <li key = {index}>{task} <button onClick = {() => {deleteTask(index)}}>delete</button></li>
+    ))}
+    </ul>
   </div>
-);
+)
 }
 export default App
