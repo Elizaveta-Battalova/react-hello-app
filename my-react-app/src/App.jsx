2dad71084aca2fefee87a7f3585261ef753c7400
Elizaveta Battalova <elizaveta.battalova@rambler.ru>
2025-06-24 21:09:42 +0300

    Добавила функционал completed для задач

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,34 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-const [tasks, setTasks] = useState([]);
-const [newTask, setNewTask] = useState('');
+ const [tasks, setTasks] = useState([]);
+ const [newTask, setNewTask] = useState('');
+ const [completedTasks, setCompletedTasks] = useState([]);
 
-const addTask = () => {
-  if(newTask.trim() !== '') {
+ const addTask = () => {
+  if (newTask.trim() !== '') {
     setTasks([...tasks, newTask.trim()]);
     setNewTask('');
   }
-};
+ };
 
-const deleteTask = (indexToDelete) => {
-  setTasks(tasks.filter((_, index) => (index !== indexToDelete)));
-};
+ const deleteTask = (indexToDelete) => {
+  setTasks(tasks.filter((_, index) => index !== indexToDelete));
+  setCompletedTasks(completedTasks.filter((_, index) => index !== indexToDelete));
+ };
 
-return(
+ const toggleCompleted = (completedIndex) => {
+  setCompletedTasks((prev) => prev.includes(completedIndex) ? prev.filter((i) => i !== completedIndex) : [...prev, completedIndex]);
+ };
+
+ return(
   <div>
-    <h1>to do list</h1>
-    <input type = 'text' placeholder = 'add task' value = {newTask} onChange = {(e) => setNewTask(e.target.value)}></input>
+    <h1>To do list</h1>
+    <input type = 'text'
+    placeholder = 'type your task'
+    value = {newTask}
+    onChange = {(e) => setNewTask(e.target.value)}></input>
     <button onClick = {addTask}>add</button>
-    <ul>
-          {tasks.map((task, index) => (
-      <li key = {index}>{task} <button onClick = {() => {deleteTask(index)}}>delete</button></li>
+
+    {tasks.map((task, index) => (
+      <li key = {index}>
+        <span onClick = {() => toggleCompleted(index)}
+        style = {{textDecoration: completedTasks.includes(index) ? 'line-through' : 'none'}}>{task}</span>
+        <button onClick = {() => deleteTask(index)}>delete</button>
+      </li>
     ))}
-    </ul>
   </div>
-)
+ )
 }
-export default App
+
+export default App;
