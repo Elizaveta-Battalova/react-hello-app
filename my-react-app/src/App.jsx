8ebac84f9d8b42b422806bd74a18d3e70da9f50b
Elizaveta Battalova <elizaveta.battalova@rambler.ru>
2025-06-25 21:32:14 +0300

    Исправлена логика удаления задач и completed

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,44 +2,49 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
- const [tasks, setTasks] = useState([]);
- const [newTask, setNewTask] = useState('');
- const [completedTasks, setCompletedTasks] = useState([]);
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+  const [completedTasks, setCompletedTasks] = useState([]);
 
- const addTask = () => {
-  if (newTask.trim() !== '') {
-    setTasks([...tasks, newTask.trim()]);
-    setNewTask('');
+  const addTask = () => {
+    if(newTask.trim() !== '') {
+      const task = {
+        id: Date.now(),
+        text: newTask.trim(),
+      }
+     setTasks([...tasks, task]);
+     setNewTask('');
+    }
   }
- };
 
- const deleteTask = (indexToDelete) => {
-  setTasks(tasks.filter((_, index) => index !== indexToDelete));
-  setCompletedTasks(completedTasks.filter((_, index) => index !== indexToDelete));
- };
+  const deleteTask = (indexToDelete) => {
+    setTasks(tasks.filter(task => task.id !== indexToDelete));
+    setCompletedTasks(completedTasks.filter(id => id !== indexToDelete));
+  }
 
- const toggleCompleted = (completedIndex) => {
-  setCompletedTasks((prev) => prev.includes(completedIndex) ? prev.filter((i) => i !== completedIndex) : [...prev, completedIndex]);
- };
+  const toggleTasks = (toggleIndex) => {
+    setCompletedTasks((prev) => prev.includes(toggleIndex) ? prev.filter((i) => i !== toggleIndex) : [...prev, toggleIndex]);
+  }
 
- return(
-  <div>
-    <h1>To do list</h1>
-    <input type = 'text'
-    placeholder = 'type your task'
-    value = {newTask}
-    onChange = {(e) => setNewTask(e.target.value)}></input>
-    <button onClick = {addTask}>add</button>
+  return(
+    <div>
+      <h1>to do list</h1>
+      <input type = 'text'
+      placeholder = 'type your task'
+      value = {newTask}
+      onChange = {(e) => setNewTask(e.target.value)}></input>
+      <button onClick = {addTask}>add task</button>
 
-    {tasks.map((task, index) => (
-      <li key = {index}>
-        <span onClick = {() => toggleCompleted(index)}
-        style = {{textDecoration: completedTasks.includes(index) ? 'line-through' : 'none'}}>{task}</span>
-        <button onClick = {() => deleteTask(index)}>delete</button>
-      </li>
-    ))}
-  </div>
- )
+      <ul>
+        {tasks.map((task) => (
+          <li key = {task.id}>
+            <span onClick = {() => toggleTasks(task.id)} style = {{textDecoration: completedTasks.includes(task.id) ? 'line-through' : 'none'}}>{task.text}</span>
+            <button onClick = {() => deleteTask(task.id)}>delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default App;
