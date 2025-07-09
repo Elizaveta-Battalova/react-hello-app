687d3dd672a1cbdcbd218636d23f6f8e35118cfd
Elizaveta Battalova <elizaveta.battalova@rambler.ru>
2025-07-09 13:53:26 +0300

    журнал еды

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,118 +2,124 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-  const [newTask, setNewTask] = useState("");
-  const [editingTaskId, setEditingTaskId] = useState(null);
-  const [editingTaskText, setEditingTaskText] = useState("");
-  const [completedTasks, setCompletedTasks] = useState([]);
-  const [loadedTasks, setLoadedTasks] = useState(false);
+const [meals, setMeals] = useState([]);
+const [newMeal, setNewMeal] = useState('');
+const [eatenMeals, setEatenMeals] = useState([]);
+const [editingMealId, setEditingMealId] = useState(null);
+const [editingMealText, setEditingMealText] = useState('');
+const [loadedMeals, setLoadedMeals] = useState(false);
+const [selectedCategory, setSelectedCategory] = useState('breakfast');
 
-  useEffect(() => {
-    const savedTasks = JSON.parse(localStorage.getItem("tasks"));
-    const savedCompletedTasks = JSON.parse(
-      localStorage.getItem("completedTasks")
-    );
+useEffect(() => {
+  try{
+    const savedMeals = JSON.parse(localStorage.getItem('meals'));
+    const savedEatenMeals = JSON.parse(localStorage.getItem('eatenMeals'));
 
-    if (savedTasks) setTasks(savedTasks);
-    if (savedCompletedTasks) setCompletedTasks(savedCompletedTasks);
+    if(savedMeals) setMeals(savedMeals);
+    if(savedEatenMeals) setEatenMeals(savedEatenMeals);
+  } catch (e) {
+    console.error('ошибка парсинга localstorage', e)}
+    setLoadedMeals(true);
+}, []);
 
-    setLoadedTasks(true);
-  }, []);
+useEffect(() => {
+  if(loadedMeals) {
+    localStorage.setItem('meals', JSON.stringify(meals));
+    localStorage.setItem('eatenMeals', JSON.stringify(eatenMeals));
+  }
+}, [meals,eatenMeals,loadedMeals]);
 
-  useEffect(() => {
-    if (loadedTasks) {
-      localStorage.setItem("tasks", JSON.stringify(tasks));
-    }
-  }, [tasks, loadedTasks]);
+const addMeal = () => {
+  const trimmedMeal = newMeal.trim();
+  if (trimmedMeal === '') return;
+  const meal = {
+    id: Date.now(),
+    text: trimmedMeal,
+    category: selectedCategory,
+  }
+  setMeals([...meals, meal]);
 
-  useEffect(() => {
-    if (loadedTasks) {
-      localStorage.setItem("completedTasks", JSON.stringify(completedTasks));
-    }
-  }, [completedTasks, loadedTasks]);
+  setNewMeal('');
+};
 
-  const addTask = () => {
-    const trimmedTask = newTask.trim();
+const startEdit = (meal) => {
+  setEditingMealId(meal.id);
+  setEditingMealText(meal.text);
+}
 
-    if (trimmedTask === "") return;
+const saveEdit = () => {
+  setMeals((prevMeals) => prevMeals.map((meal) => meal.id === editingMealId ? {...meal, text: editingMealText} : meal));
+  setEditingMealId(null);
+  setEditingMealText('');
+}
 
-    const task = {
-      id: Date.now(),
-      text: trimmedTask,
-    };
+const cancelEdit = () => {
+  setEditingMealId(null);
+  setEditingMealText('');
+}
 
-    setTasks([...tasks, task]);
-    setNewTask("");
-  };
+const toggleEatenMeal = (idToToggle) => {
+  setEatenMeals((prevEatenMeals) => prevEatenMeals.includes(idToToggle) ? prevEatenMeals.filter((id) => id !== idToToggle) : [...prevEatenMeals, idToToggle]);
+}
 
-  const startEdit = (task) => {
-    setEditingTaskId(task.id);
-    setEditingTaskText(task.text);
-  };
+const deleteMeal = (idToDelete) => {
+  setMeals((prevMeal) => prevMeal.filter((meal) => meal.id !== idToDelete));
+  setEatenMeals((prevEatenMeal) => prevEatenMeal.filter((id) => id !== idToDelete));
+}
 
-  const saveEdit = () => {
-    setTasks(
-      tasks.map((task) =>
-        task.id === editingTaskId ? { ...task, text: editingTaskText } : task
-      )
-    );
-    setEditingTaskId(null);
-    setEditingTaskText("");
-  };
 
-  const cancelEdit = () => {
-    setEditingTaskId(null);
-    setEditingTaskText("");
-  };
-
-  const toggleCompletedTask = (completedId) => {
-    setCompletedTasks((current) =>
-      current.includes(completedId)
-        ? current.filter((id) => id !== completedId)
-        : [...current, completedId]
-    );
-  };
-
-  const deleteTask = (idToDelete) => {
-    setTasks(tasks.filter((task) => task.id !== idToDelete));
-  };
-
-  return (
-    <div>
-      <h1>Список задач</h1>
-      <div className="input-wrapper">
-       <input
-        type="text"
-        placeholder="Напишите задачу"
-        value={newTask}
-        onChange={(e) => setNewTask(e.target.value)}
-        onKeyDown={(e) => e.key === "Enter" && addTask()}
-      />
-      <button type="button" onClick={addTask} style={{ backgroundColor: "green", color: "white" }}>
-         ✓
-      </button>
-      </div>
-
-      <ul>
-        {tasks.map((task) => (
-          <li key = {task.id}
-          className = {completedTasks.includes(task.id) ? 'completed' : ''}>
-            {task.id === editingTaskId ? (<>
-            <input type = 'text'
-            value = {editingTaskText}
-            onChange = {(e) => setEditingTaskText(e.target.value)}/>
-            <button type="button" onClick = {saveEdit} style={{ backgroundColor: "green", color: "white" }}>Сохранить</button>
-            <button type="button" onClick = {cancelEdit} style={{ backgroundColor: "gray", color: "white" }}>Отмена</button></>) : (<>
-            <span onClick = {() => toggleCompletedTask(task.id)}
-              style = {{textDecoration: completedTasks.includes(task.id) ? 'line-through' : 'none', cursor: 'pointer'}}>{task.text}</span>
-              <button type="button" onClick = {() => startEdit(task)} style={{ backgroundColor: "orange", color: "white" }}>Редактировать</button></>)}
-              <button ензу = "button" onClick = {() => deleteTask(task.id)} style = {{bacgroundColor: "red", color: "white"}}>✗</button>
-          </li>
-        ))}
-      </ul>
+return(
+  <div>
+    <h1>Дневник еды</h1>
+    <div className = 'input-wrapper'>
+      <select value = {selectedCategory}
+      onChange = {(e) => setSelectedCategory(e.target.value)}>
+        <option value = 'breakfast'>Завтрак</option>
+        <option value = 'lunch'>Обед</option>
+        <option value = 'dinner'>Ужин</option>
+      </select>
+      <input type = 'text'
+      placeholder = 'Запишите вашу еду'
+      value = {newMeal}
+      onChange = {(e) => setNewMeal(e.target.value)}
+      onKeyDown = {(e) => e.key === 'Enter' && addMeal()}/>
+      <button type = 'button' onClick = {addMeal}>Добавить</button>
     </div>
-  );
+    <div>
+      {['breakfast', 'lunch', 'dinner'].map((category) => {
+        const categoryMeals = meals.filter((meal) => meal.category === category);
+        if(categoryMeals.length === 0) return null;
+        return(
+          <div key = {category}>
+            <h2>
+              {category === 'breakfast' && 'Завтрак'}
+              {category === 'lunch' && 'Обед'}
+              {category === 'dinner' && 'Ужин'}
+            </h2>
+                <ul>
+      {categoryMeals.map((meal) => (
+        <li key = {meal.id}
+            className = {eatenMeals.includes(meal.id) ? 'completed' : ''}>
+            {meal.id === editingMealId ? (<>
+            <input type = 'text'
+            value = {editingMealText}
+            onChange = {(e) => setEditingMealText(e.target.value)}/>
+
+            <button type = 'button' onClick = {saveEdit} style = {{backgroundColor: 'green', cursor: 'pointer'}}>✓</button>
+            <button type = 'button' onClick = {cancelEdit} style = {{backgroundColor: 'gray', cursor: 'pointer'}}>⨉</button></>) : (<>
+            <span onClick = {() => toggleEatenMeal(meal.id)} style = {{textDecoration: eatenMeals.includes(meal.id) ? 'line-through' : 'none'}}>{meal.text}{' '}</span>
+
+            <button type = 'button' onClick = {() => startEdit(meal)} style = {{display: eatenMeals.includes(meal.id) ? 'none' : 'block', backgroundColor: 'orange', cursor: 'pointer'}}>Ред.</button>
+            <button type = 'button' onClick = {() => deleteMeal(meal.id)} style = {{display: eatenMeals.includes(meal.id) ? 'none' : 'block'}}>⨉</button></>)}
+        </li>
+      ))}
+    </ul>
+          </div>
+        )
+      })}
+    </div>
+  </div>
+)
 }
 
 export default App;
